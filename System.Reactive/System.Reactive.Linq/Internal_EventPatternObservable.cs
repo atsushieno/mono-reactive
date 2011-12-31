ec89a05d58d16c3b7385e7e288d9dbbeb32f41ee
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2011-12-31 13:03:09 +0900

    implemented non-TDelegate and converter-supplied FromEventPattern() methods.

---
+++ System.Reactive/System.Reactive.Linq/Internal_EventPatternObservable.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+
+namespace System.Reactive.Linq
+{
+	class EventObservable2<THandlerType, T> : IObservable<T>
+	{
+		Func<ISubject<T>, THandlerType> handler_creator;
+		Action<THandlerType> add_handler;
+		Action<THandlerType> remove_handler;
+		
+		public EventObservable2 (Func<ISubject<T>, THandlerType> handlerCreator, Action<THandlerType> addHandler, Action<THandlerType> removeHandler)
+		{
+			handler_creator = handlerCreator;
+			add_handler = addHandler;
+			remove_handler = removeHandler;
+		}
+		
+		public IDisposable Subscribe (IObserver<T> observer)
+		{
+			var sub = new Subject<T> ();
+			var handler = handler_creator (sub);
+			add_handler (handler);
+			var dis = sub.Subscribe (observer);
+			return Disposable.Create (() => { dis.Dispose (); remove_handler (handler); });
+		}
+	}
+
+	class EventPatternObservableGeneric<TEventArgs> : IObservable<EventPattern<TEventArgs>> where TEventArgs : EventArgs
+	{
+		Action<EventHandler<TEventArgs>> add_handler;
+		Action<EventHandler<TEventArgs>> remove_handler;
+		
+		public EventPatternObservableGeneric (Action<EventHandler<TEventArgs>> addHandler, Action<EventHandler<TEventArgs>> removeHandler)
+		{
+			add_handler = addHandler;
+			remove_handler = removeHandler;
+		}
+		
+		public IDisposable Subscribe (IObserver<EventPattern<TEventArgs>> observer)
+		{
+			var sub = new Subject<EventPattern<TEventArgs>> ();
+			var handler = new EventHandler<TEventArgs> ((o, e) => sub.OnNext (new EventPattern<TEventArgs> (o, e)));
+			add_handler (handler);
+			var dis = sub.Subscribe (observer);
+			return Disposable.Create (() => { dis.Dispose (); remove_handler (handler); });
+		}
+	}
+
+	class EventPatternObservableNonGeneric : IObservable<EventPattern<EventArgs>>
+	{
+		Action<EventHandler> add_handler;
+		Action<EventHandler> remove_handler;
+		
+		public EventPatternObservableNonGeneric (Action<EventHandler> addHandler, Action<EventHandler> removeHandler)
+		{
+			add_handler = addHandler;
+			remove_handler = removeHandler;
+		}
+		
+		public IDisposable Subscribe (IObserver<EventPattern<EventArgs>> observer)
+		{
+			var sub = new Subject<EventPattern<EventArgs>> ();
+			var handler = new EventHandler ((o, e) => sub.OnNext (new EventPattern<EventArgs> (o, e)));
+			add_handler (handler);
+			var dis = sub.Subscribe (observer);
+			return Disposable.Create (() => { dis.Dispose (); remove_handler (handler); });
+		}
+	}
+
+	class EventPatternObservable<TDelegate, TEventArgs> : IObservable<EventPattern<TEventArgs>> where TEventArgs : EventArgs
+	{
+		Func<EventHandler<TEventArgs>, TDelegate> handler_creator;
+		Action<TDelegate> add_handler;
+		Action<TDelegate> remove_handler;
+		
+		public EventPatternObservable (Func<EventHandler<TEventArgs>, TDelegate> handlerCreator, Action<TDelegate> addHandler, Action<TDelegate> removeHandler)
+		{
+			handler_creator = handlerCreator;
+			add_handler = addHandler;
+			remove_handler = removeHandler;
+		}
+		
+		public IDisposable Subscribe (IObserver<EventPattern<TEventArgs>> observer)
+		{
+			var sub = new Subject<EventPattern<TEventArgs>> ();
+			var handler = handler_creator ((o, ea) => sub.OnNext (new EventPattern<TEventArgs> (o, ea)));
+			add_handler (handler);
+			var dis = sub.Subscribe (observer);
+			return Disposable.Create (() => { dis.Dispose (); remove_handler (handler); });
+		}
+	}
+
+	// FIXME: This is no-go (see code that uses this)
+	class EventPatternObservable2<THandlerType, TEventArgs> : IObservable<EventPattern<TEventArgs>> where TEventArgs : EventArgs
+	{
+		Func<ISubject<EventPattern<TEventArgs>>, THandlerType> handler_creator;
+		Action<THandlerType> add_handler;
+		Action<THandlerType> remove_handler;
+		
+		public EventPatternObservable2 (Func<ISubject<EventPattern<TEventArgs>>, THandlerType> handlerCreator, Action<THandlerType> addHandler, Action<THandlerType> removeHandler)
+		{
+			handler_creator = handlerCreator;
+			add_handler = addHandler;
+			remove_handler = removeHandler;
+		}
+		
+		public IDisposable Subscribe (IObserver<EventPattern<TEventArgs>> observer)
+		{
+			var sub = new Subject<EventPattern<TEventArgs>> ();
+			var handler = handler_creator (sub);
+			add_handler (handler);
+			var dis = sub.Subscribe (observer);
+			return Disposable.Create (() => { dis.Dispose (); remove_handler (handler); });
+		}
+	}
+}
--- System.Reactive/System.Reactive.Linq/Observable.cs
+++ System.Reactive/System.Reactive.Linq/Observable.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Reactive;
 using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
@@ -447,7 +448,19 @@ namespace System.Reactive.Linq
 		public static IObservable<TEventArgs> FromEvent<TDelegate, TEventArgs> (
 			Action<TDelegate> addHandler,
 			Action<TDelegate> removeHandler)
-		{ throw new NotImplementedException (); }
+		// TDelegate must be a delegate that only takes a TEventArgs (no "object sender")
+		// FIXME: How can I implement this guy???
+		{
+			throw new NotImplementedException ();
+
+			/* this is no-go, fails to bind for incompatible argument types (EventArgs vs. TEventArgs !) */
+			ISubject<TEventArgs> subject;
+			Action<TEventArgs> d = delegate (TEventArgs args) { subject.OnNext (args); };
+			Func<ISubject<TEventArgs>, TDelegate> handlerCreator =
+				sub => { subject = sub; return (TDelegate) (object) Delegate.CreateDelegate (typeof (TDelegate), d.Method); };
+
+			return new EventObservable2<TDelegate, TEventArgs> (handlerCreator, addHandler, removeHandler);
+		}
 		
 		public static IObservable<TEventArgs> FromEvent<TDelegate, TEventArgs> (
 			Func<Action<TEventArgs>, TDelegate> conversion,
@@ -461,43 +474,102 @@ namespace System.Reactive.Linq
 			Action<EventHandler<TEventArgs>> addHandler,
 			Action<EventHandler<TEventArgs>> removeHandler)
 			where TEventArgs : EventArgs
-		{ throw new NotImplementedException (); }
+		{
+			return new EventPatternObservableGeneric<TEventArgs> (addHandler, removeHandler);
+		}
 		
 		public static IObservable<EventPattern<EventArgs>> FromEventPattern (
 			Action<EventHandler> addHandler,
 			Action<EventHandler> removeHandler)
-		{ throw new NotImplementedException (); }
+		{
+			return new EventPatternObservableNonGeneric (addHandler, removeHandler);
+		}
 
 		public static IObservable<EventPattern<TEventArgs>> FromEventPattern<TDelegate, TEventArgs> (
 			Action<TDelegate> addHandler,
 			Action<TDelegate> removeHandler)
 			where TEventArgs : EventArgs
-		{ throw new NotImplementedException (); }
+		{
+			/* FIXME: this is no-go, fails to bind for incompatible argument types (EventArgs vs. TEventArgs !) */
+			ISubject<EventPattern<TEventArgs>> subject;
+			Action<object, TEventArgs> d = delegate (object sender, TEventArgs args) { subject.OnNext (new EventPattern<TEventArgs> (sender, args)); };
+			Func<ISubject<EventPattern<TEventArgs>>, TDelegate> handlerCreator =
+				sub => { subject = sub; return (TDelegate) (object) Delegate.CreateDelegate (typeof (TDelegate), d.GetType ().GetMethod ("Invoke")); };
+
+			return new EventPatternObservable2<TDelegate, TEventArgs> (handlerCreator, addHandler, removeHandler);
+		}
 		
 		public static IObservable<EventPattern<EventArgs>> FromEventPattern (
 			object target,
 			string eventName)
-		{ throw new NotImplementedException (); }
+		{
+			if (target == null)
+				throw new ArgumentNullException ("target");
+			if (eventName == null)
+				throw new ArgumentNullException ("eventName");
+
+			var type = target.GetType ();
+			var evt = type.GetEvent (eventName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+			return FromEventInfoNonGeneric (evt, target);
+		}
 		
 		public static IObservable<EventPattern<TEventArgs>> FromEventPattern<TEventArgs> (
 			object target,
 			string eventName)
 			where TEventArgs : EventArgs
-		{ throw new NotImplementedException (); }
+		{
+			if (target == null)
+				throw new ArgumentNullException ("target");
+			if (eventName == null)
+				throw new ArgumentNullException ("eventName");
+
+			var type = target.GetType ();
+			var evt = type.GetEvent (eventName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+			return FromEventInfoGeneric<TEventArgs> (evt, target);
+		}
 		
 		public static IObservable<EventPattern<EventArgs>> FromEventPattern (Type type, string eventName)
-		{ throw new NotImplementedException (); }
+		{
+			if (type == null)
+				throw new ArgumentNullException ("type");
+			if (eventName == null)
+				throw new ArgumentNullException ("eventName");
+
+			var evt = type.GetEvent (eventName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+			return FromEventInfoNonGeneric (evt, null);
+		}
+		
+		static IObservable<EventPattern<EventArgs>> FromEventInfoNonGeneric (EventInfo info, object target)
+		{
+			return new EventPatternObservableNonGeneric (handler => info.AddEventHandler (target, handler), handler => info.RemoveEventHandler (target, handler));
+		}
+		
+		static IObservable<EventPattern<TEventArgs>> FromEventInfoGeneric<TEventArgs> (EventInfo info, object target) where TEventArgs : EventArgs
+		{
+			return new EventPatternObservableGeneric<TEventArgs> (handler => info.AddEventHandler (target, handler), handler => info.RemoveEventHandler (target, handler));
+		}
 		
 		public static IObservable<EventPattern<TEventArgs>> FromEventPattern<TEventArgs> (Type type, string eventName)
 			where TEventArgs : EventArgs
-		{ throw new NotImplementedException (); }
+		{
+			if (type == null)
+				throw new ArgumentNullException ("type");
+			if (eventName == null)
+				throw new ArgumentNullException ("eventName");
+
+			var evt = type.GetEvent (eventName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+			return FromEventInfoGeneric<TEventArgs> (evt, null);
+		}
 		
 		public static IObservable<EventPattern<TEventArgs>> FromEventPattern<TDelegate, TEventArgs> (
 			Func<EventHandler<TEventArgs>, TDelegate> conversion,
 			Action<TDelegate> addHandler,
 			Action<TDelegate> removeHandler)
 			where TEventArgs : EventArgs
-		{ throw new NotImplementedException (); }
+		{
+			return new EventPatternObservable<TDelegate, TEventArgs> (conversion, addHandler, removeHandler);
+		}
+
 
 		public static IObservable<TResult> Generate<TState, TResult> (
 			TState initialState,
