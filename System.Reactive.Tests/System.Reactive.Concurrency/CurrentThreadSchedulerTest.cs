7ee8f922b5f1bf72607f169d21b92f8a6c10c111
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2012-01-22 18:21:54 +0900

    Add CurrentThreadScheduler test that *sometimes* used to fail due to concurrency.

---
+++ System.Reactive.Tests/System.Reactive.Concurrency/CurrentThreadSchedulerTest.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+namespace System.Reactive.Concurrency.Tests
+{
+	[TestFixture]
+	public class CurrentThreadSchedulerTest
+	{
+		[Test]
+		public void Concurrency ()
+		{
+			// this is to test some race condition in scheduling (which used to fail)
+			for (int i = 0; i < 100; i++)
+				RunConcurrencyTest ();
+		}
+		
+		void RunConcurrencyTest ()
+		{
+			var stream = Observable.Range (0, 9).Do (TextWriter.Null.WriteLine); // ... is done on CurrentThreadScheduler.
+			var source = stream.ObserveOn (Scheduler.ThreadPool);
+			bool done = false;
+			var dis = source.Subscribe (TextWriter.Null.WriteLine, () => done = true);
+			SpinWait.SpinUntil (() => done == true, 1000);
+			dis.Dispose ();
+		}
+	}
+}
