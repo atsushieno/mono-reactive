3c3ee042794be726fe054f0c6c137e6777cd809f
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2012-01-29 16:52:45 +0900

    Add Publish() and re-Connect()ing test.

--- System.Reactive.Tests/System.Reactive.Linq/ObservableConnectableTest.cs
+++ System.Reactive.Tests/System.Reactive.Linq/ObservableConnectableTest.cs
@@ -51,5 +51,26 @@ namespace System.Reactive.Linq.Tests
 			Assert.AreEqual (cdis1, cdis2, "#2");
 			cdis1.Dispose ();
 		}
+		[Test] // FIXME: this test is somewhat processing-speed dependent. Sleep() is not enough very often.
+		public void PublishReconnect ()
+		{
+			var source = Observable.Interval (TimeSpan.FromMilliseconds (50));
+
+			int result = 0;
+			bool started = false;
+			var published = source.Publish ();
+			var pdis1 = published.Subscribe (i => result++);
+			var cdis1 = published.Connect ();
+			Thread.Sleep (200); // should be enough to receive some events
+			Assert.IsTrue (result > 0, "#1");
+			cdis1.Dispose (); // disconnect
+			int oldResult = result;
+			Thread.Sleep (200); // should be enough to raise interval event if it were active (which should *not*)
+			Assert.AreEqual (oldResult, result, "#2");
+			var cdis2 = published.Connect ();
+			Thread.Sleep (400); // should be enough to receive some events
+			Assert.IsTrue (result > oldResult, "#3");
+			cdis2.Dispose ();
+		}
 	}
 }
