cc5d351473822dcf974dfc3e05d77c73829447c8
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2012-02-05 15:29:51 +0900

    Started Nunit test helper implementation (untested).

--- Mono.Reactive.Testing/Mono.Reactive.Testing.cs
+++ Mono.Reactive.Testing/Mono.Reactive.Testing.cs
@@ -4,8 +4,10 @@ using System.Globalization;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using NUnit.Framework;
 
 namespace Mono.Reactive.Testing
 {
@@ -24,46 +26,98 @@ namespace Mono.Reactive.Testing
 	{
 		public static void AreElementsEqual<T> (IEnumerable<T> expected, IEnumerable<T> actual)
 		{
-			throw new NotImplementedException ();
+			AreElementsEqual (expected, actual, "Enumerated items are not equal");
 		}
 		
 		public static void AreElementsEqual<T> (IObservable<T> expected, IObservable<T> actual)
 		{
-			throw new NotImplementedException ();
+			AreElementsEqual (expected, actual, "Observed items are not equal");
 		}
 		
 		public static void AreElementsEqual<T> (IEnumerable<T> expected, IEnumerable<T> actual, string message)
 		{
-			throw new NotImplementedException ();
+			if (expected == null) {
+				if (actual != null)
+					throw new ArgumentNullException ("expected");
+				else
+					return;
+			}
+			var ee = expected.GetEnumerator ();
+			var ae = actual.GetEnumerator ();
+			int i = 0;
+			for (; ee.MoveNext (); i++) {
+				if (!ae.MoveNext ())
+					Assert.Fail (String.Format ("{0} (Insufficient items, ended at index {1})", message, i));
+				Assert.AreEqual (ee.Current, ae.Current, String.Format ("{0} (items at index {1})", message, i));
+			}
+			if (ae.MoveNext ())
+				Assert.Fail (String.Format ("{0} (Extra items, after index {1})", message, i));
+		}
+		
+		struct Indexed<T>
+		{
+			public Indexed (int i, T value)
+			{
+				index = i;
+				this.value = value;
+			}
+			int index;
+			T value;
+			
+			public int Index { get { return index; } }
+			public T Value { get { return value; } }
 		}
 		
 		public static void AreElementsEqual<T> (IObservable<T> expected, IObservable<T> actual, string message)
 		{
-			throw new NotImplementedException ();
+			if (expected == null) {
+				if (actual != null)
+					throw new ArgumentNullException ("expected");
+				else
+					return;
+			}
+
+			int ie = 0, ia = 0, endE = 0, endA = 0;
+			var iex = expected.Select (e => new Indexed<T> (ie++, e)).Finally (() => endE = ie);
+			var iac = actual.Select (e => new Indexed<T> (ia++, e)).Finally (() => endA = ia);
+			var source = iex.Zip (iac, (e, a) => { Assert.AreEqual (e.Value, a.Value, String.Format ("{0} (Items differ at index {1})", message, e.Index)); return Unit.Default; });
+			var dis = new SingleAssignmentDisposable ();
+			dis.Disposable = source.Finally<Unit> (() => dis.Dispose ()).Subscribe (v => {}, () => Assert.AreEqual (endE, endA, String.Format ("{0} (Items counts differ: expected {1} but got {2})", endE, endA)));
 		}
 		
 		public static void Throws<TException> (Action action)
 			where TException : Exception
 		{
-			throw new NotImplementedException ();
+			Throws<TException> (action, "Should raise " + typeof (TException));
 		}
 		
 		public static void Throws<TException> (Action action, string message)
 			where TException : Exception
 		{
-			throw new NotImplementedException ();
+			try {
+				action ();
+				Assert.Fail (message);
+			} catch (Exception ex) {
+				// FIXME: should this be IsAssignableFrom() ?
+				Assert.AreEqual (typeof (TException), ex.GetType (), message);
+			}
 		}
 		
 		public static void Throws<TException> (TException exception, Action action)
 			where TException : Exception
 		{
-			throw new NotImplementedException ();
+			Throws<TException> (exception, action, "Should raise " + typeof (TException));
 		}
 		
 		public static void Throws<TException> (TException exception, Action action, string message)
 			where TException : Exception
 		{
-			throw new NotImplementedException ();
+			try {
+				action ();
+				Assert.Fail (message);
+			} catch (Exception ex) {
+				Assert.AreEqual (exception, ex, message);
+			}
 		}
 	}
 	
@@ -75,27 +129,27 @@ namespace Mono.Reactive.Testing
 
 		public static Recorded<Notification<T>> OnCompleted<T> (long ticks)
 		{
-			throw new NotImplementedException ();
+			return new Recorded<Notification<T>> (ticks, Notification.CreateOnCompleted<T> ());
 		}
 		
 		public static Recorded<Notification<T>> OnError<T> (long ticks, Exception exception)
 		{
-			throw new NotImplementedException ();
+			return new Recorded<Notification<T>> (ticks, Notification.CreateOnError<T> (exception));
 		}
 		
 		public static Recorded<Notification<T>> OnNext<T> (long ticks, T value)
 		{
-			throw new NotImplementedException ();
+			return new Recorded<Notification<T>> (ticks, Notification.CreateOnNext<T> (value));
 		}
 		
 		public static Subscription Subscribe (long start)
 		{
-			throw new NotImplementedException ();
+			return new Subscription (start);
 		}
 		
 		public static Subscription Subscribe (long start, long end)
 		{
-			throw new NotImplementedException ();
+			return new Subscription (start, end);
 		}
 	}
 
@@ -200,10 +254,44 @@ namespace Mono.Reactive.Testing
 
 	public class TestScheduler : VirtualTimeScheduler<long, long>
 	{
+		static readonly Comparer<long> long_comparer = Comparer<long>.Default;
+		static readonly Comparison<Task> comparison = (t1, t2) => long_comparer.Compare (t1.DueTime, t2.DueTime);
+		
+		List<Task> tasks = new List<Task> ();
+
+		// VirtualTimeScheduler members.
+		
 		protected override long Add (long absolute, long relative)
 		{
 			return absolute + relative;
 		}
+		
+		protected override DateTimeOffset ToDateTimeOffset (long absolute)
+		{
+			return new DateTimeOffset (new DateTime (absolute));
+		}
+		
+		protected override long ToRelative (TimeSpan timeSpan)
+		{
+			return timeSpan.Ticks;
+		}
+		
+		class Task
+		{
+			public long DueTime;
+			public Func<IDisposable> Action;
+		}
+		
+		public override IDisposable ScheduleAbsolute<TState> (TState state, long dueTime, Func<IScheduler, TState, IDisposable> action)
+		{
+			Task t = default (Task);
+			t = new Task () { DueTime = dueTime, Action = () => { tasks.Remove (t); return action (this, state); }};
+			tasks.Add (t);
+			tasks.Sort (comparison);
+			return new CompositeDisposable (Disposable.Create (() => tasks.Remove (t)));
+		}
+		
+		// TestScheduler specific.
 		
 		public ITestableObservable<T> CreateColdObservable<T> (params Recorded<Notification<T>> [] messages)
 		{
@@ -216,11 +304,6 @@ namespace Mono.Reactive.Testing
 		}
 		
 		public ITestableObserver<T> CreateObserver<T> ()
-		{
-			throw new NotImplementedException ();
-		}
-		
-		public override IDisposable ScheduleAbsolute<TState> (TState state, long dueTime, Func<IScheduler, TState, IDisposable> action)
 		{
 			throw new NotImplementedException ();
 		}
@@ -238,16 +321,6 @@ namespace Mono.Reactive.Testing
 		public ITestableObserver<T> Start<T> (Func<IObservable<T>> create, long created, long subscribed, long disposed)
 		{
 			throw new NotImplementedException ();
-		}
-		
-		protected override DateTimeOffset ToDateTimeOffset (long absolute)
-		{
-			return new DateTimeOffset (new DateTime (absolute));
-		}
-		
-		protected override long ToRelative (TimeSpan timeSpan)
-		{
-			return timeSpan.Ticks;
 		}
 	}
 }
