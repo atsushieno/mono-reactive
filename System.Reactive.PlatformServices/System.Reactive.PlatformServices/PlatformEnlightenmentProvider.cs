426db0f805bdddfecabc1a308835c8b7febcb036
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2012-03-11 18:01:33 +0900

    cosmetic API fix for PlatformEnlightenmentProvider.

--- System.Reactive.PlatformServices/System.Reactive.PlatformServices/PlatformEnlightenmentProvider.cs
+++ System.Reactive.PlatformServices/System.Reactive.PlatformServices/PlatformEnlightenmentProvider.cs
@@ -1,16 +1,18 @@
 #if REACTIVE_2_0
 using System;
+using System.ComponentModel;
 using System.Reactive;
 
 namespace System.Reactive.PlatformServices
 {
+	[EditorBrowsable (EditorBrowsableState.Never)]
 	public class PlatformEnlightenmentProvider : IPlatformEnlightenmentProvider
 	{
 		public PlatformEnlightenmentProvider ()
 		{
 		}
 
-		public T GetService<T> (params object[] args) where T : class
+		public T GetService<T> (object[] args) where T : class
 		{
 			throw new NotImplementedException ();
 		}
