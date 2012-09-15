6d2445058651457eb69a0f311111d5e84a73592a
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2012-09-15 23:39:37 +0900

    extract assembly version name info into another source.

---
+++ Common/CommonAssemblyInfo.cs
@@ -0,0 +1,9 @@
+namespace System
+{
+	internal class CommonAssemblyInfo
+	{
+		public const string Version = "2.0.20814.0";
+		public const string PublicKeyToken = "f300afd708cefcd3";
+		public const string CoreAssemblyName = "System.Reactive.Core, Version=" + Version + ", Culture=neutral, PublicKeyToken=" + PublicKeyToken;
+	}
+}
--- System.Reactive.Core/System.Reactive.PlatformServices/PlatformEnlightenmentProvider.cs
+++ System.Reactive.Core/System.Reactive.PlatformServices/PlatformEnlightenmentProvider.cs
@@ -6,14 +6,11 @@ namespace System.Reactive.PlatformServices
 	[EditorBrowsable (EditorBrowsableState.Advanced)]
 	public static class PlatformEnlightenmentProvider
 	{
-		const string version = "2.0.20814.0";
-		const string publickeytoken = "System.Reactive.Core, Version=2.0.20823.0, Culture=neutral, PublicKeyToken=f300afd708cefcd3";
 		static IPlatformEnlightenmentProvider current;
 		
 		static PlatformEnlightenmentProvider ()
 		{
-			var type = Type.GetType (String.Format ("System.Reactive.PlatformServices.CurrentPlatformEnlightenmentProvider, System.Reactive.PlatformServices, Version={0}, Culture=neutral, PublicKeyToken={1}",
-			                         version, publickeytoken));
+			var type = Type.GetType (CommonAssemblyInfo.CoreAssemblyName);
 			if (type == null)
 				throw new InvalidOperationException ("Platform services assembly could not be loaded");
 
